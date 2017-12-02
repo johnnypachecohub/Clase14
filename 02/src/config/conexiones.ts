d297ac3248d3fa88d4bddf7344f6db7de41df740
Brian Nuñez <idbrianur@gmail.com>
2017-12-02 14:23:31 -0500

    Clase 14

---
+++ 02/src/config/conexiones.ts
@@ -0,0 +1,5 @@
+const usuarioMongo = "johnny"
+const claveMongo = "jplserver"
+const conexionMongo: string = `mongodb://${usuarioMongo}:${claveMongo}@ds127936.mlab.com:27936/baseprueba`
+
+export {conexionMongo}
--- 02/src/servidor.ts
+++ 02/src/servidor.ts
@@ -1,13 +1,19 @@
 // Declaraciones
 import express = require("express")
 import {Application, Request, Response, NextFunction} from 'express'
+import bodyParser = require("body-parser")
+import methodOverride = require("method-override")  // es un middleware
+import * as morgan from 'morgan'		//es otro middleware (morgan no bloquea nada, solo registra)
 
 // Settings
 const app:Application = express()
-
+app.set("view engine", "pug")
+app.set("views", "./vistas")
 
 // Middlewares
-
+app.use(morgan("dev"))
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(methodOverride("_method"))
 
 // Rutas
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
@@ -17,8 +23,29 @@ app.get("/", (req: Request, res: Response, next: NextFunction) => {
 		.send("todo ok")
 })
 
+app.post("/", (req: Request, res: Response, next: NextFunction) => {
+	res
+		.status(200)
+		.type("application/json")
+		.send(JSON.stringify(req.body))
+})
+
+app.put("/registro", (req: Request, res: Response, next: NextFunction) => {
+	res
+		.status(200)
+		.type("text/plain")
+		.send("Enviado usando el metodo PUT")
+})
+
+app.get("/formulario", (req: Request, res: Response, next: NextFunction) => {
+	res.render("formulario")
+})
 
 // El servidor
 app.listen(4000, () => {
 	console.log("Ejecutando en el puerto 4000")
 })
+
+
+//http://localhost:4000/formulario
+//http://localhost:4000/registro?_method=PUT
